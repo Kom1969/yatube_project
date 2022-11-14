925081fc5d82e6f23c4a088d7bc22e031d2ee91e
Michail Koryukin <kom1969@list.ru>
2022-11-14 14:17:55 +0500

    Добавлены urls и view-функции~

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+# Страница с информацией об публикациях;
+# view-функция принимает параметр slug из path()
+def group_posts(request, slug):
+    return HttpResponse(f'Публикация {slug}') 
