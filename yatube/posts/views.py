b809f8e0102b0bf27ef5a26f0b64136d07edb43e
Michail Koryukin <kom1969@list.ru>
2022-11-22 22:01:06 +0500

    Проект изменение 1~

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
 
 # Страница с информацией об публикациях;
 # view-функция принимает параметр slug из path()
 def group_posts(request, slug):
-    return HttpResponse(f'Публикация {slug}') 
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_posts.html', context) 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -51,10 +51,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +129,5 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',    
 ]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
